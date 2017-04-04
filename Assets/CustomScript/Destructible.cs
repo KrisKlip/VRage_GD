c3cee4717390ba7921cea36b7dd1bf690c66455a
KrisKlip <knklipfel@gmail.com>
2017-04-04 00:05:30 -0600

    TABLES

---
+++ Assets/CustomScript/Destructible.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Destructible : MonoBehaviour {
+	public GameObject debrisPrefab;
+
+	void onMouseDown(){
+		DestroyMe();
+	}
+	void DestroyMe(){
+		if (debrisPrefab) {
+			Instantiate (debrisPrefab, transform.position, transform.rotation);
+
+		}
+		Destroy (gameObject);
+		debrisPrefab.transform.position = new Vector3(0, 0, 0);
+	}
+	void onCollisionEnter( Collision collision){
+		
+	}
+	void Update(){
+		DestroyMe ();
+	}
+}
